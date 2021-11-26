3ffd7288e791b0b84c80549fdba903ca4d6aefae
imdnk <imdnk96@gmail.com>
2021-11-26 15:03:03 +0530

    Spacer

--- src/features/components/RestaurantsInfoCard.js
+++ src/features/components/RestaurantsInfoCard.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 import {  Card, Paragraph, Title as PaperTitle} from "react-native-paper";
 import styled from "styled-components";
 import { SvgXml } from "react-native-svg";
 
 import star from "../../../assets/star";
-import open from "../../../assets/open"
+import open from "../../../assets/open";
+import { Spacer } from "../../components/SpaceComponent/Spacer";
 
 
 const CardCover = styled(Card.Cover)`
@@ -38,27 +39,28 @@ const Section = styled(View)`
 `;
 
 const Rating = styled(View)`
-   
+   flex: 1 ;
    flexDirection: row;
-   paddingTop: ${(props) => props.theme.space[2]}
+   paddingTop: ${(props) => props.theme.space[2]};
 `;
 
 const SectionEnd = styled(View)`
-   flex: 1;
    flexDirection: row;
    justifyContent: flex-end;
-   paddingTop: ${(props) => props.theme.space[2]}
+   paddingTop: ${(props) => props.theme.space[2]};
+   paddingLeft: 8px;
 `;
 export const RestaurantsInfoCard = (restaurant = {}) => {
   const {
     name = "DNK Restaurant",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     address = "Perundurai Road, Erode",
-    photos = [{uri: 'https://lh3.googleusercontent.com/LwldymTId-YhaZtpGnRMxMwk2_1CYkYxjYoF8G89c_kOviI4qTTc_H0MAQD_DwbaJaZkF43ReKv3UvB05JF-fHgMGl8=w1000'}],
+    photos = [{uri: 'https://lh3.googleusercontent.com/LwldymTId-YhaZtpGnRMxMwk2_1CYkYxjYoF8G89c_kOviI4qTTc_H0MAQD_DwbaJaZkF43ReKv3UvB05JF-fHgMGl8=w1000'},{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwS70r6aZEg6-wofSf66x7MU7FiZSEFSOIQA&usqp=CAU'}],
     IsOpenNow = true,
-    IsTemperiryClosed = false,
+    IsTemperiryClosed = true,
     rating =4,
   } = restaurant;
+
 
   const ratingArray = Array.from(new Array(Math.floor(rating)));
 
@@ -70,10 +72,16 @@ export const RestaurantsInfoCard = (restaurant = {}) => {
         <Section>
         <Rating>
         {ratingArray.map( () => <SvgXml xml = {star} width = '20' height = '20'/>)}
-        <SectionEnd>
-        {IsOpenNow && <SvgXml xml = {open} width = '20' height = '20' />}
-        </SectionEnd>
         </Rating>
+        <SectionEnd>
+          <Text style = {{ color: 'red', fontWeight: 'bold'}}>TEMPORARILY CLOSED</Text>
+        </SectionEnd>
+        <Spacer position ='left' size = 'large'>
+        {IsOpenNow && <SvgXml xml = {open} width = '20' height = '20' />}
+        </Spacer>
+        <Spacer position ='left' size = 'large'>
+        {IsTemperiryClosed && <Image style = {{width: 20,height: 20}} source = {{uri: icon}} /> }
+        </Spacer>
         </Section>
         <Address>{address}</Address>
       </CardInfo>
